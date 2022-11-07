d80ff486d5a6f13dafa190f6495fe14bc548dbcc
David Ginn <daginn@microsoft.com>
2022-11-07 10:12:26 -0500

    fixes login flow to load home page on first request

--- src/authenticated.js
+++ src/authenticated.js
@@ -6,6 +6,7 @@ import PatientDashboard from "./components/patientDashboard";
 import Physicians from "./components/physicians";
 
 function Authenticated() {
+
   return (
     <>
       <BrowserRouter>
@@ -14,11 +15,12 @@ function Authenticated() {
             <Route path="/patients/:patientId" element={<Patient />} />
             <Route path='/physicians' element={<Physicians />} />
             <Route path="/patients/:patientId/dashboard" element={<PatientDashboard />} />
-            <Route path="/" element={<PatientDashboard />} />
+            <Route path="/dashboard" element={<PatientDashboard />} />
+            <Route path="/" element={<PatientDashboard  />} />
         </Routes>
       </BrowserRouter>
     </>
   );
-}
+} 
 
 export default Authenticated;
