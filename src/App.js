230a7b7186e5ff241d1c7a599009ef879d7e89e4
tz <66768111+timzhan@users.noreply.github.com>
2023-10-08 17:08:38 +0800

    Remove Authentications in app.js and index.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,34 @@
+// import "./App.css";
+// import {
+//   useMsalAuthentication,
+//   AuthenticatedTemplate,
+//   UnauthenticatedTemplate,
+// } from "@azure/msal-react";
+// import { InteractionType } from "@azure/msal-browser";
+// import { loginRequest } from "./authConfig";
+// import Authenticated from "./authenticated";
+// import MainLayout from "./layout/mainLayout";
+
+// function App() {
+//   useMsalAuthentication(InteractionType.Redirect, loginRequest);
+
+//   return (
+//     <div className="App">
+//       <MainLayout>
+//         <AuthenticatedTemplate>
+//           <Authenticated />
+//         </AuthenticatedTemplate>
+//         <UnauthenticatedTemplate>
+//           <p>Please wait while you are signed in.</p>
+//         </UnauthenticatedTemplate>
+//       </MainLayout>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
 import "./App.css";
 import {
   useMsalAuthentication,
@@ -5,22 +36,22 @@ import {
   UnauthenticatedTemplate,
 } from "@azure/msal-react";
 import { InteractionType } from "@azure/msal-browser";
-import { loginRequest } from "./authConfig";
-import Authenticated from "./authenticated";
+// import { loginRequest } from "./authConfig";
+// import Authenticated from "./authenticated";
 import MainLayout from "./layout/mainLayout";
 
 function App() {
-  useMsalAuthentication(InteractionType.Redirect, loginRequest);
+  // useMsalAuthentication(InteractionType.Redirect, loginRequest);
 
   return (
     <div className="App">
       <MainLayout>
-        <AuthenticatedTemplate>
+        {/* <AuthenticatedTemplate>
           <Authenticated />
         </AuthenticatedTemplate>
         <UnauthenticatedTemplate>
           <p>Please wait while you are signed in.</p>
-        </UnauthenticatedTemplate>
+        </UnauthenticatedTemplate> */}
       </MainLayout>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,33 +1,54 @@
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import App from "./App";
+// import reportWebVitals from "./reportWebVitals";
+// import { PublicClientApplication } from "@azure/msal-browser";
+// import { MsalProvider } from "@azure/msal-react";
+// import { msalConfig } from "./authConfig";
+
+// export const msalInstance = new PublicClientApplication(msalConfig);
+
+// // Default to using the first account if no account is active on page load
+// if (
+//   !msalInstance.getActiveAccount() &&
+//   msalInstance.getAllAccounts().length > 0
+// ) {
+//   // Account selection logic is app dependent. Adjust as needed for different use cases.
+//   msalInstance.setActiveAccount(msalInstance.getAllAccounts()[0]);
+// }
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(
+//   <React.StrictMode>
+//     <MsalProvider instance={msalInstance}>
+//       <App />
+//     </MsalProvider>
+//   </React.StrictMode>
+// );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
+
+
+
+// Remove the authentication
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { PublicClientApplication } from "@azure/msal-browser";
-import { MsalProvider } from "@azure/msal-react";
-import { msalConfig } from "./authConfig";
-
-export const msalInstance = new PublicClientApplication(msalConfig);
-
-// Default to using the first account if no account is active on page load
-if (
-  !msalInstance.getActiveAccount() &&
-  msalInstance.getAllAccounts().length > 0
-) {
-  // Account selection logic is app dependent. Adjust as needed for different use cases.
-  msalInstance.setActiveAccount(msalInstance.getAllAccounts()[0]);
-}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <MsalProvider instance={msalInstance}>
+  // <React.StrictMode>
+  //   <MsalProvider instance={msalInstance}>
       <App />
-    </MsalProvider>
-  </React.StrictMode>
+  //   </MsalProvider>
+  // </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
