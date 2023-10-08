fc3a362e84818050b6f16bcec367e4536f76ab5f
tz <66768111+timzhan@users.noreply.github.com>
2023-10-08 15:04:58 +0800

    updated authority

--- src/authConfig.js
+++ src/authConfig.js
@@ -2,7 +2,7 @@ export const msalConfig = {
   auth: {
     clientId: "644c1acc-4c8e-474c-b2a0-5609d04dc947",
     authority:
-      "https://login.microsoftonline.com/",
+      "https://login.microsoftonline.com/c2c1d092-cf24-4636-a284-203c93601579",
     redirectUri: "http://localhost:3000",
   },
   cache: {
