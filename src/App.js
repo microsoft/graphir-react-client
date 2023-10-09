9ea09dde3ec8e53e749a9ddb1ae1476f0806a5b6
tz <66768111+timzhan@users.noreply.github.com>
2023-10-09 10:04:40 +0800

    Removed the auth codes from App.js and index.js

--- src/App.js
+++ src/App.js
@@ -30,18 +30,10 @@
 
 
 import "./App.css";
-import {
-  useMsalAuthentication,
-  AuthenticatedTemplate,
-  UnauthenticatedTemplate,
-} from "@azure/msal-react";
-import { InteractionType } from "@azure/msal-browser";
-// import { loginRequest } from "./authConfig";
-// import Authenticated from "./authenticated";
+
 import MainLayout from "./layout/mainLayout";
 
 function App() {
-  // useMsalAuthentication(InteractionType.Redirect, loginRequest);
 
   return (
     <div className="App">
